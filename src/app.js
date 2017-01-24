83993794b5fb347c4e42b7709d3fd60683afecc4
Dinesh Yenneti <goyennet@microsoft.com>
2017-01-24 20:32:55 +0800

    Added list widget

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import $ from 'jquery';
-import * as jqueryUi from 'jquery-ui';
 import * as ko from "knockout";
+import * as _ from 'underscore';
 
 require("./pages/common/list-select/list-select");
 
 $(() => {
-    ko.applyBindings({}, $("#mainContainer")[0]);
+    let selectItems = _.map(_.range(15), (item) => {
+        return {
+            id: item, 
+            text: "item" + item
+        }
+    })
+
+    let listSelectOptions = {
+        title: "Sample",
+        items: ko.observableArray(selectItems)
+    }
+
+    ko.applyBindings({
+        listSelectOptions,
+    }, $("#mainContainer")[0]);
 }) 
 
 import css from 'style-loader!css-loader!less-loader!./assets/css/style.less';
--- src/pages/common/list-select/list-select.js
+++ src/pages/common/list-select/list-select.js
@@ -1,10 +1,35 @@
 import * as ko from "knockout";
+import $ from 'jquery';
+import * as _ from 'underscore';
 
 var template = require("raw!./list-select.html");
 
 class ListSelectViewModel {
     constructor(params) {
         this.title = params.title;
+        this.items = ko.observableArray(this.getItems(params));
+
+        this.click = (dataItem, event) => {
+            this.onSelect(dataItem, event);
+        }
+    }
+
+    getItems(params) {
+        let items = params.items().length ? params.items().slice() : [];
+
+        items = _.map(items, (item, index) => {
+            return _.defaults(item, {
+                css: "",
+                index
+            });
+        });
+
+        return items;
+    }
+
+    onSelect(dataItem, event) {
+        $("#" + this.title + " li").removeClass("active");
+        $($("#" + this.title + " li")[dataItem.index]).addClass("active");
     }
 }
 
